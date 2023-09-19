2ae43f09edfc8d4f0cd3e487a32671ab19c20e9d
Lyrdinn <lyrdinn@gmail.com>
2023-09-19 15:08:44 +0200

    Fix fichiers

---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <Windows.h>
+
+void InitializeBuffer()
+{
+    const int SCREEN_WIDTH = 200;
+    const int SCREEN_HEIGHT = 200;
+
+    HANDLE hOutput = (HANDLE)GetStdHandle(STD_OUTPUT_HANDLE);
+
+    COORD dwBufferSize = { SCREEN_WIDTH,SCREEN_HEIGHT };
+    COORD dwBufferCoord = { 0, 0 };
+    SMALL_RECT rcRegion = { 0, 0, SCREEN_WIDTH - 1, SCREEN_HEIGHT - 1 };
+
+    CHAR_INFO buffer[SCREEN_HEIGHT][SCREEN_WIDTH];
+
+    ReadConsoleOutput(hOutput, (CHAR_INFO*)buffer, dwBufferSize, dwBufferCoord, &rcRegion);
+
+    buffer[5][10].Char.AsciiChar = 'H';
+    buffer[5][10].Attributes = 0x0E;
+    buffer[5][11].Char.AsciiChar = 'i';
+    buffer[5][11].Attributes = 0x0B;
+    buffer[5][12].Char.AsciiChar = '!';
+    buffer[5][12].Attributes = 0x0A;
+
+    WriteConsoleOutput(hOutput, (CHAR_INFO*)buffer, dwBufferSize, dwBufferCoord, &rcRegion);
+}
+
+
+int main()
+{
+    InitializeBuffer();
+    return 0;
+}
