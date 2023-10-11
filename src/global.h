7eda44c33460486a71a52e1f3996aabace50c470
Lyrdinn <90039770+Lyrdinn@users.noreply.github.com>
2023-10-11 16:08:33 +0200

    Pixel art font

--- src/global.h
+++ src/global.h
@@ -1,13 +1,13 @@
 #pragma once
 
-#define SCREEN_WIDTH 150
+#define SCREEN_WIDTH 200
 #define SCREEN_HEIGHT 150
 
 #define BUTTON_WIDTH 3
 #define BUTTON_HEIGHT 3
 
-#define TILE_WIDTH 3
-#define TILE_HEIGHT 3
+#define TILE_WIDTH 6
+#define TILE_HEIGHT 6
 
 #define LEVEL_WIDTH (SCREEN_WIDTH / TILE_WIDTH)
 #define LEVEL_HEIGHT (SCREEN_HEIGHT / TILE_HEIGHT)
--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,21 @@ using namespace std;
 
 int main()
 {
+    //Resize de la fenetre pour qu'elle soit en grand écran obligatoire ainsi qu'en mode pixel art.
+    keybd_event(VK_MENU, 0x38, 0, 0);
+    keybd_event(VK_RETURN, 0x1c, 0, 0);
+    keybd_event(VK_RETURN, 0x1c, KEYEVENTF_KEYUP, 0);
+    keybd_event(VK_MENU, 0x38, KEYEVENTF_KEYUP, 0);
+
+    CONSOLE_FONT_INFOEX cfi;
+    cfi.cbSize = sizeof(cfi);
+    cfi.nFont = 0;
+    cfi.dwFontSize.X = 10;
+    cfi.dwFontSize.Y = 8;
+    cfi.FontFamily = FF_DONTCARE;
+    cfi.FontWeight = FW_NORMAL;
+    SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), FALSE, &cfi);
+
     Screen screen = Screen();
     MenuScene menuScene =  MenuScene(&screen);
     LevelSelectScene levelSelectScene =  LevelSelectScene(&screen);
